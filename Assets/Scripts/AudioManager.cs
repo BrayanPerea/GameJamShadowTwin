f69a3778cdb1cd0ed77b8a414ec5f27cb7bf6bdc
Daniel Bazurto <dbg1209@gmail.com>
2024-07-09 21:00:38 -0500

    UI Elements, Updated Scripts
    Added UI Scene for main menu and options
    Added TextMeshPro items
    Created GameManager and AudioManager Scripts
    HotFix Updated the PlayerController script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum SoundType
+{
+    STEPS,
+    JUMP,
+    LANDING,
+    HURT,
+    GAMEOVER,
+    GAMEWIN
+}
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip[] soundList;
+    
+    private void Awake() 
+    {
+        instance = this;   
+
+        DontDestroyOnLoad(this); 
+    }
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   private Rigidbody playerRb;
+
     public float horizontalInput;
     public float forwardInput;
-    public float speed = 20.0f;
+    public float speed = 5.0f;
+    private Animator playerAnim;
+    public float jumpForce = 10;
+    public float gravityModifier= 1.5f;
+    public bool isOnGround = true;
+    public bool isMoving= false;
 
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;    
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right*horizontalInput* Time.deltaTime*speed);
+       /* horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Forward");
-        transform.Translate(Vector3.forward* forwardInput*Time.deltaTime*speed);
+        */
+       
+
+        if(Input.GetKey(KeyCode.S)){
+            transform.Translate(Vector3.back*horizontalInput* Time.deltaTime*speed);
+            playerAnim.SetFloat("Speed", -1f);
+
+        }else if(Input.GetKey(KeyCode.W)){
+            transform.Translate(Vector3.forward* forwardInput*Time.deltaTime*speed);
+            playerAnim.SetFloat("Speed", 1f);
+
+        }else{
+            playerAnim.SetFloat("Speed", 0f);
+        }
+        
+            if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetBool("Jumping", true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            playerAnim.SetBool("Jumping", false);
+        }
     }
 }
